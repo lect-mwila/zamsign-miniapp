de7dce10782ec0de22742ce72ff13186fa76e6c4
Mwila Nyirongo <mpnyirongo@gmail.com>
2026-03-26 10:16:01 +0200

    test-deploy-react

--- vite.config.ts
+++ vite.config.ts
@@ -4,18 +4,25 @@ import mkcert from 'vite-plugin-mkcert';
 import { readFileSync } from 'node:fs';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'node:path';
+// import { tanstackRouter } from '@tanstack/router-plugin/vite'
 
 const config = defineConfig({
   base: process.env.GH ? '/zamsign-miniapp/': '/',
   plugins: [
     tsconfigPaths({ projects: ['./tsconfig.json'] }),
     process.env.HTTPS && mkcert(),
+	/**
+	 tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+    }),
+  **/
   ],
    server: process.env.REMOTE ? {
      host: 'zamsign.com',
      https: {
-       cert: readFileSync(resolve('.perms/zamsign.com+2.pem')), // <-- YOUR CERT
-       key: readFileSync(resolve('.perms/zamsign.com+2-key.pem')), //  <-- YOUR KEY
+		key: readFileSync(resolve('.perms/zamsign.com+2-key.pem')), //  <-- YOUR KEY
+		cert: readFileSync(resolve('.perms/zamsign.com+2.pem')), // <-- YOUR CERT
      },
      port: 443
    } : {
